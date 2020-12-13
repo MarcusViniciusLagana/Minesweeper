56bb610c95ccb55581be95b6d99d506a9de2af5b
MarcusViniciusLagana <71972975+MarcusViniciusLagana@users.noreply.github.com>
2020-12-13 16:02:20 -0300

    Second mouse button working

--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square (props) {
-    const name = props.clicked + ' square';
-    let value = props.value;
-
+    const name = props.state.clicked + ' square';
+    let value = props.state.value;
     if (value === 0) value = '';
-
-    //if (props.clicked) name += props.clicked;
-    //if (props.clicked) name = 'clicked square'
+    const clickHandle = (mouse) => props.clickHandle(mouse);
+    const contextHandle = (mouse) => {mouse.preventDefault(); props.clickHandle(mouse);}
 
     return (
-        <button className={name} onClick={props.clickHandle}>
+        <button className={name} onClick={clickHandle} onContextMenu={contextHandle}>
             {value}
         </button>
     );
 }
 
 function BoardRow (props) {
-    const values = props.values;
-    const clicked = props.clicked;
-    const size = values.length;
+    const size = props.state.values.length;
     const squares = [];
 
     for (let i = 0; i < size; i++) {
-        squares.push(<Square value={values[i]} clicked={clicked[i]} clickHandle={() => props.clickHandle(i)}/>);
+        const value = props.state.values[i];
+        const clicked = props.state.clicked[i];
+        const state = {value, clicked};
+        const clickHandle = (mouse) => props.clickHandle(mouse, i);
+        squares.push(<Square state={state} clickHandle={clickHandle}/>);
     }
 
     return(
@@ -37,12 +37,15 @@ function BoardRow (props) {
 
 function Board (props) {
     const size = props.state.size;
-    const values = props.state.values;
-    const clicked = props.state.clicked;
+
     const rows = [];
 
     for (let i = 0; i < size; i++) {
-        rows.push(<BoardRow values={values.slice(i * size, i * size + size)} clicked={clicked.slice(i * size, i * size + size)} clickHandle={(j) => props.clickHandle(i * size + j)}/>);
+        const values = props.state.values.slice(i * size, i * size + size);
+        const clicked = props.state.clicked.slice(i * size, i * size + size);
+        const state = {values, clicked};
+        const clickHandle = (mouse, j) => props.clickHandle(mouse, i * size + j);
+        rows.push(<BoardRow state={state} clickHandle={clickHandle}/>);
     }
 
     return (
@@ -58,11 +61,11 @@ class Game extends React.Component {
         super(props);
         const mines = [];
         const bombs = ['\u2620','\u2622','\u2623'];
-        const safe = '\u26A0';
 
-        for (let i=0; i < props.size + 2; i++) {
+        for (let i=0; i < props.size + 1; i++) {
             const index = Math.floor(Math.random() * props.size ** 2);
             if (!mines.includes(index)) mines.push(index);
+            else i--;
         }
 
         const index = Math.floor(Math.random() * 3);
@@ -71,7 +74,7 @@ class Game extends React.Component {
             size: props.size,
             values: Array(props.size ** 2).fill(null),
             clicked: Array(props.size ** 2).fill(''),
-            symbols: [safe, bombs[index]],
+            bomb: bombs[index],
             mines
         };
     }
@@ -79,11 +82,11 @@ class Game extends React.Component {
     restartGame() {
         const mines = [];
         const bombs = ['\u2620','\u2622','\u2623'];
-        const safe = '\u26A0';
 
         for (let i=0; i < this.props.size + 2; i++) {
             const index = Math.floor(Math.random() * this.props.size ** 2);
             if (!mines.includes(index)) mines.push(index);
+            else i--;
         }
 
         const index = Math.floor(Math.random() * 3);
@@ -92,30 +95,40 @@ class Game extends React.Component {
             size: this.props.size,
             values: Array(this.props.size ** 2).fill(null),
             clicked: Array(this.props.size ** 2).fill(''),
-            symbols: [safe, bombs[index]],
+            bomb: bombs[index],
             mines
         });
     }
 
-    clickHandle (index) {
+    clickHandle (mouse, index) {
         const values = this.state.values;
         const clicked = this.state.clicked;
         const mines = this.state.mines;
+        const status = ['saved-exploded','maybe',''];
+        const symbols = ['\u26A0', '?', ''];
         let positions = [];
 
-        if (clicked[index]) return;
+        if (mouse.button === 0) {
+            if (clicked[index]) return;
 
-        if (mines.includes(index)) {
-            clicked[index] = 'clicked saved-exploded';
-            values[index] = this.state.symbols[1];
-        } else [values[index], clicked[index], positions] = this.countBombs(index, this.state.size, mines);
+            if (mines.includes(index)) {
+                clicked[index] = 'clicked saved-exploded';
+                values[index] = this.state.bomb;
+            } else [values[index], clicked[index], positions] = this.countBombs(index, this.state.size, mines);
+        } else if (mouse.button === 2) {
+            if (!status.includes(clicked[index])) return;
+
+            let i = status.indexOf(clicked[index]) + 1;
+            if (i === status.length) i = 0;
+            clicked[index] = status[i];
+            values[index] = symbols[i];
+        }
 
         this.setState({ values, clicked });
 
-        if (values[index] === 0) {
-            for (const i of positions) this.clickHandle(i);
+        if (mouse.button === 0 && values[index] === 0) {
+            for (const i of positions) this.clickHandle(mouse, i);
         }
-
     }
 
     countBombs (index, size, mines) {
@@ -162,7 +175,7 @@ class Game extends React.Component {
             <div className="game-info">{'\u2691 \u2B59 \u26A0 \u2BD1 ? \u2753 \u2620 \u2622 \u2623'}</div>
             <div className="game-area">
                 <div className="game">
-                    <Board state={this.state} clickHandle={(i) => this.clickHandle(i)}/>
+                    <Board state={this.state} clickHandle={(mouse, i) => this.clickHandle(mouse, i)}/>
                 </div>  
                 <div className="restart">
                     <button className="restart-button" onClick={() => this.restartGame()}>Restart Game</button>
