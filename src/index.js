f0750f59fc949c8403c1a62a023ee9824232261f
MarcusViniciusLagana <71972975+MarcusViniciusLagana@users.noreply.github.com>
2020-12-13 00:50:44 -0300

    Added symbols

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 function Square (props) {
     let name = 'square';
 
-    if (props.clicked) name = 'clicked square'
+    if (props.clicked) name = 'clicked ' + name + ' ' + props.clicked;
+    //if (props.clicked) name = 'clicked square'
 
     return (
         <button className={name} onClick={props.clickHandle}>
@@ -52,39 +53,66 @@ class Game extends React.Component {
 
     constructor(props) {
         super(props);
+        const mines = [];
+        const bombs = ['\u2620','\u2622','\u2623'];
+        const safe = '\u26A0';
+
+        for (let i=0; i < props.size + 1; i++) {
+            const index = Math.floor(Math.random() * props.size ** 2);
+            if (!mines.includes(index)) mines.push(index);
+        }
+
+        const index = Math.floor(Math.random() * 3);
+
         this.state = {
             size: props.size,
             values: Array(props.size ** 2).fill(null),
-            clicked: Array(props.size ** 2).fill(false),
-            mines: Array(props.size + 1).fill(Math.floor(Math.random() * props.size ** 2))
+            clicked: Array(props.size ** 2).fill(null),
+            symbols: [safe, bombs[index]],
+            mines
         };
     }
 
     restartGame() {
+        const mines = [];
+        const bombs = ['\u2620','\u2622','\u2623'];
+        const safe = '\u26A0';
+
+        for (let i=0; i < this.props.size + 1; i++) {
+            const index = Math.floor(Math.random() * this.props.size ** 2);
+            if (!mines.includes(index)) mines.push(index);
+        }
+
+        const index = Math.floor(Math.random() * 3);
+
         this.setState({
             size: this.props.size,
             values: Array(this.props.size ** 2).fill(null),
-            clicked: Array(this.props.size ** 2).fill(false),
-            mines: Array(this.props.size + 1).fill(Math.floor(Math.random() * this.props.size ** 2))
-        }); 
+            clicked: Array(this.props.size ** 2).fill(null),
+            symbols: [safe, bombs[index]],
+            mines
+        });
     }
 
     clickHandle (index) {
-        //const values = this.state.values;
+        const values = this.state.values;
         const clicked = this.state.clicked;
+        const mines = this.state.mines;
 
         if (clicked[index]) return;
 
-        clicked[index] = true;
-        console.log(index);
+        if (mines.includes(index)) {
+            clicked[index] = 'saved-exploded';
+            values[index] = this.state.symbols[1];
+        } else clicked[index] = 'clicked'
 
-        this.setState({ clicked });
+        this.setState({ values, clicked });
     }
 
     render() {
         return (<div>
             <div className="title">Minesweeper</div>
-            <div className="game-info">Info</div>
+            <div className="game-info">{'\u2691 \u2B59 \u26A0 \u2BD1 ? \u2753 \u2620 \u2622 \u2623'}</div>
             <div className="game-area">
                 <div className="game">
                     <Board state={this.state} clickHandle={(i) => this.clickHandle(i)}/>
