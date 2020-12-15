044d77f11a1d834c0e280bdf191ae6afe7ca3d33
MarcusViniciusLagana <71972975+MarcusViniciusLagana@users.noreply.github.com>
2020-12-15 12:03:39 -0300

    Code clean and improvement

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square (props) {
-    const name = props.state.clicked + ' square';
+    const cssClass = props.state.cssClass + ' square';
     let value = props.state.value;
     const clickHandle = (mouse) => props.clickHandle(mouse);
+    const contextHandle = (mouse) => {mouse.preventDefault(); props.clickHandle(mouse);}
 
     return (
-        <button className={name} onClick={clickHandle} onContextMenu={clickHandle}>
+        <button className={cssClass} onClick={clickHandle} onContextMenu={contextHandle}>
             {value}
         </button>
     );
@@ -20,8 +21,8 @@ function BoardRow (props) {
 
     for (let i = 0; i < size; i++) {
         const value = props.state.values[i];
-        const clicked = props.state.clicked[i];
-        const state = {value, clicked};
+        const cssClass = props.state.cssClass[i];
+        const state = {value, cssClass};
         const clickHandle = (mouse) => props.clickHandle(mouse, i);
         squares.push(<Square state={state} clickHandle={clickHandle}/>);
     }
@@ -40,8 +41,8 @@ function Board (props) {
 
     for (let i = 0; i < size; i++) {
         const values = props.state.values.slice(i * size, i * size + size);
-        const clicked = props.state.clicked.slice(i * size, i * size + size);
-        const state = {values, clicked, size};
+        const cssClass = props.state.cssClass.slice(i * size, i * size + size);
+        const state = {values, cssClass, size};
         const clickHandle = (mouse, j) => props.clickHandle(mouse, i * size + j);
         rows.push(<BoardRow state={state} clickHandle={clickHandle}/>);
     }
@@ -53,60 +54,88 @@ function Board (props) {
     );
 }
 
+function Menu (props) {
+    return (
+        <div className="game-info game">
+            <div className="bombs-time">
+                {Zerofill(props.bombs,3)}
+            </div>
+            <div className="level">
+                B
+            </div>
+            <div className="bombs-time">
+                {Zerofill(props.time,3)}
+            </div>
+        </div>
+    );
+}
+
 class Game extends React.Component {
 
     static timerID = null;
 
     constructor(props) {
         super(props);
-        const mines = [];
-        const bombs = ['\u2620','\u2622','\u2623'];
         const size = props.size;
 
-        for (let i=0; i < size + 1; i++) {
+        // sorting mines positions
+        const mines = Array(size + 1);
+        for (let i=0; i < mines.length; i++) {
             const index = Math.floor(Math.random() * size ** 2);
-            if (!mines.includes(index)) mines.push(index);
+            if (!mines.includes(index)) mines[i] = index;
             else i--;
         }
 
+        // sorting bomb symbol
+        const bombs = ['\u2620','\u2622','\u2623'];
         const index = Math.floor(Math.random() * bombs.length);
 
+        // setting initial state
         this.state = {
-            values: Array(size ** 2).fill(null),
-            clicked: Array(size ** 2).fill(''),
+            values: Array(size ** 2).fill(''),
+            cssClass: Array(size ** 2).fill(''),
             bomb: bombs[index],
             size,
             mines,
             phase: 'paused',
             initialTime: props.time,
-            time: props.time,
+            time: props.time
         };
     }
 
+    // when mounting, set timer to null
     componentDidMount() { Game.timerID = null; }
 
+    // when unmouting, reset timer
     componentWillUnmount() { if (Game.timerID) clearInterval(Game.timerID); }
 
-    restartGame() {
-        const mines = [];
-        const bombs = ['\u2620','\u2622','\u2623'];
-        const time = this.state.initialTime;
+    restartGame(size = null) {
+        // reseting size
+        if (!size) size = this.state.size;
 
-        for (let i=0; i < this.props.size + 1; i++) {
-            const index = Math.floor(Math.random() * this.props.size ** 2);
-            if (!mines.includes(index)) mines.push(index);
+        // sorting mines positions
+        const mines = Array(size + 1);
+        for (let i=0; i < mines.length; i++) {
+            const index = Math.floor(Math.random() * size ** 2);
+            if (!mines.includes(index)) mines[i] = index;
             else i--;
         }
 
-        const index = Math.floor(Math.random() * 3);
-
+        // resseting timer
+        const time = this.state.initialTime;
         if (Game.timerID) clearInterval(Game.timerID);
         Game.timerID = null;
 
+        // sorting new bomb symbol
+        const bombs = ['\u2620','\u2622','\u2623'];
+        const index = Math.floor(Math.random() * bombs.length);
+
+        // reseting state
         this.setState({
-            values: Array(this.props.size ** 2).fill(null),
-            clicked: Array(this.props.size ** 2).fill(''),
+            values: Array(size ** 2).fill(''),
+            cssClass: Array(size ** 2).fill(''),
             bomb: bombs[index],
+            size,
             mines,
             phase: 'paused',
             time
@@ -114,13 +143,19 @@ class Game extends React.Component {
     }
 
     clickHandle (mouse, index) {
-        const values = this.state.values;
-        const clicked = this.state.clicked;
-        const mines = this.state.mines;
+        let values = this.state.values;
+        let cssClass = this.state.cssClass;
         let phase = this.state.phase;
-        const symbols = [null, '\u2691', '?', null];
         let positions = [];
 
+        if (phase === 'game-over') return;
+
+        if (!this.state.time) {
+            this.setState({ phase: 'game-over' });
+            return;
+        }
+
+        // If it is the first click, initializes clock
         if (phase === 'paused') {
             phase = 'playing'
             if (!Game.timerID) Game.timerID = setInterval(() => {
@@ -131,115 +166,123 @@ class Game extends React.Component {
             },1000);
         }
 
+        // if clicked with left button
         if (mouse.button === 0) {
-            if (clicked[index]) return;
+            // if square was already clicked, then return.
+            if (cssClass[index]) return;
 
-            if (mines.includes(index)) {
-                clicked[index] = 'clicked red';
+            // if square is a bomb, game-over
+            if (this.state.mines.includes(index)) {
+                cssClass[index] = 'clicked exploded';
                 values[index] = this.state.bomb;
-                // Lógica de detonar as bombas
+                phase = 'game-over';
+                clearInterval(Game.timerID);
+            // if square is not a bomb, count bombs around the square
+            // Update value with the number of bombs and cssClass with
+            // 'clicked ' + the number of bombs as text
+            // positions keep the indexes of the squares around
             } else {
-                [values[index], clicked[index], positions] = this.countBombs(index);
+                [values[index], cssClass[index], positions] = CountBombs(this.state.size, this.state.mines, index);
                 if (values[index] === 0) values[index] = '';
             }
-            if (!values[index]) for (const i of positions) this.clickHandle(mouse, i);
+            // if square has no bomb around, open all the squares around
+            if (!values[index]) for (const i of positions) [values, cssClass] = OpenSquares(i, values, cssClass, this.state.mines, this.state.size);
+        // if clicked with right button
         } else if (mouse.button === 2) {
-            mouse.preventDefault()
-            if (clicked[index] && clicked[index] !== 'blue') return;
+            // if the button is clicked, return
+            if (cssClass[index] && cssClass[index] !== 'saved') return;
 
-            const i = symbols.indexOf(values[index]) + 1;
-            values[index] = symbols[i];
-            if (i === 1) clicked[index] = 'blue';
-            else clicked[index] = '';
+            // Cycle through the symbols '' (nothing), '\u2691' (saved) and '?' (maybe) with each click
+            values[index] = values[index] === '' ? '\u2691' : values[index] === '\u2691' ? '?' : '';
+            // set the corresponding cssClass if the saved symbol is used
+            if (values[index] === '\u2691') cssClass[index] = 'saved';
+            else cssClass[index] = '';
         }
 
-        this.setState({ values, clicked, phase });
-    }
-
-    countBombs (index) {
-        const size = this.state.size;
-        const mines = this.state.mines;
-        const words = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-        let positions = [index - size - 1, index - size, index - size + 1, index - 1, index + 1, index + size -1, index + size, index + size + 1];
-        const borderA = [];
-        const borderB = [];
-        let border = [];
-        const indexes = [];
-
-        // Which indexes are at the borders
-        for (let i=0; i < size; i++) {
-            borderA.push(i * size);
-            borderB.push(i * size + size - 1);
+        // Check if only the bomb squares are not clicked, if yes -> game-over
+        if (this.state.mines.length === cssClass.filter(x => x.indexOf('clicked') === -1).length) {
+            phase = 'game-over';
+            clearInterval(Game.timerID);
         }
-        // If the index is in one of the borders, we don't want to count
-        // bombs that are in the other border
-        if (borderA.includes(index)) border = borderB;
-        if (borderB.includes(index)) border = borderA;
 
-        // Find itens that are above or below the board
-        // and itens that are in the other border
-        for (let i=0; i < 8; i++) {
-            if (positions[i] < 0) indexes.push(i);
-            else if (border.includes(positions[i])) indexes.push(i);
-            else if (positions[i] > size ** 2 - 1) indexes.push(i);
-        }
-        // Remove indexes that we don't want to count bombs
-        for (const i of indexes) {
-            positions.splice(i, 1, '');
-        }
-        positions = positions.filter(x => x !== '');
-
-        // Count bombs in adjacent squares
-        let bombs = 0;
-        for (const i of positions) if (mines.includes(i)) bombs++;
-
-        return([bombs, 'clicked ' + words[bombs], positions])
+        // save the current state
+        this.setState({ values, cssClass, phase });
     }
 
     render() {
         const mines = this.state.mines;
-        const clicked = this.state.clicked;
-        const bombs = mines.length - clicked.length + clicked.filter(x => x !== 'blue').length;
-        const time = this.state.time;
+        const cssClass = this.state.cssClass;
+        // Count the number of mines already discovered (saved)
+        const bombs = mines.length - cssClass.filter(x => x === 'saved').length;
 
         return (<div>
             <div className="title">Minesweeper</div>
             <div className="game-area">
-                <Menu bombs={bombs} time={time}/>
+                <Menu bombs={bombs} time={this.state.time}/>
                 <div className="game">
                     <Board state={this.state} clickHandle={(mouse, i) => this.clickHandle(mouse, i)}/>
                 </div>  
                 <div className="restart">
-                    <button className="restart-button" onClick={() => this.restartGame(120)}>Restart Game</button>
+                    <button className="restart-button" onClick={() => this.restartGame()}>Restart Game</button>
                 </div>
             </div>
         </div>);
     }
 }
 
-//class timeHandler {
-//    static timer = null;
-//    static initialTime = 120;
-//    static time = 10;
-//}
+function OpenSquares (index, values, cssClass, mines, size) {
+    let positions = [];
+    // if square was already clicked, then return.
+    if (cssClass[index]) return [values, cssClass];
 
-function Menu (props) {
-    return (
-        <div className="game-info game">
-            <div className="bombs-time">
-                {zeroFill(props.bombs,3)}
-            </div>
-            <div className="level">
-                B
-            </div>
-            <div className="bombs-time">
-                {zeroFill(props.time,3)}
-            </div>
-        </div>
-    );
+    // Count bombs around the square, update value with the number of bombs
+    // and cssClass with 'clicked ' + the number of bombs as text
+    // positions keep the indexes of the squares around
+    [values[index], cssClass[index], positions] = CountBombs(size, mines, index);
+    if (values[index] === 0) values[index] = '';
+
+    // if square has no bomb around, open all the squares around
+    if (!values[index]) for (const i of positions) [values, cssClass] = OpenSquares(i, values, cssClass, mines, size);
+
+    // return the updated values and cssClasses -> return them to clickHandle to update state
+    return [values, cssClass];
 }
 
-function zeroFill( number, width ) {
+function CountBombs (size, mines, index) {
+    const cssClasses = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+    let positions = [index - size - 1, index - size, index - size + 1, index - 1, index + 1, index + size -1, index + size, index + size + 1];
+    const borderA = Array(size);
+    const borderB = Array(size);
+    let border = [];
+
+    // Which indexes are at the borders
+    for (let i=0; i < size; i++) {
+        borderA[i] = i * size;
+        borderB[i] = (i + 1) * size - 1;
+    }
+    
+    // If the index is in one of the borders, we don't want to count
+    // bombs that are in the other border
+    if (borderA.includes(index)) border = borderB;
+    if (borderB.includes(index)) border = borderA;
+
+    // Find itens that are above or below the board
+    // and itens that are in the other border and remove them
+    for (let i=0; i < 8; i++) {
+        if (positions[i] < 0 || border.includes(positions[i]) || positions[i] > size ** 2 - 1)
+            positions.splice(i, 1, '');
+    }
+    positions = positions.filter(x => x !== '');
+
+    // Count bombs in adjacent squares
+    let bombs = 0;
+    for (const i of positions) if (mines.includes(i)) bombs++;
+
+    // return the number of bombs, the updated cssClass and the valid positions around index
+    return([bombs, 'clicked ' + cssClasses[bombs], positions])
+}
+
+function Zerofill (number,width) {
     let sign = '';
 
     if (number < 0) {
@@ -250,6 +293,8 @@ function zeroFill( number, width ) {
     width -= number.toString().length - 1;
     if ( width > 0 )
         return sign + new Array(width).join('0') + number;
+    
+    // Always return a string
     return sign + number + "";
 }
 
